af44a28e0bb93fa0c5bcfb1dd7ccc5bf9d36c5cb
Yusra <yusra-tahir@hotmail.co.uk>
2022-05-17 00:23:30 +0100

    added styling for game setup page
    added some tests for Lobby and Form

---
+++ frontend/src/components/Form/Form.test.js
@@ -0,0 +1,36 @@
+import { default as Form } from ".";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+describe("Form", () => {
+  let getResultMock;
+
+  beforeEach(() => {
+    getResultMock = jest.fn();
+    render(<Form getResult={getResultMock} />);
+  });
+
+  test("it renders a form", () => {
+    let form = screen.getByLabel("game-selection");
+    expect(form).toBeInTheDocument();
+  });
+  test("it renders a category dropdown menu", () => {
+    let categoryInput = screen.getByLabelText("category");
+    expect(categoryInput).toBeInTheDocument();
+  });
+
+  test("it calls category choice on form submission", () => {
+    let categoryInput = screen.getByLabelText("category");
+    userEvent.type(categoryInput, "Music{enter}");
+    expect(getResultMock).toHaveBeenNthCalledWith(1, "Music");
+  });
+
+  test("it renders a number of questions option", () => {
+    let numberOfQuestionsInput = screen.getByLabelText("number of questions");
+    expect(numberOfQuestionsInput).toBeInTheDocument();
+  });
+  test("it calls number of questions chosen on form submission", () => {
+    let numberOfQuestionsInput = screen.getByLabelText("number of questions");
+    userEvent.type(numberOfQuestionsInput, "5{enter}");
+    expect(getResultMock).toHaveBeenNthCalledWith(1, "5");
+  });
+});
--- frontend/src/pages/SetGame/SetGame.test.js
+++ frontend/src/pages/SetGame/SetGame.test.js
@@ -3,6 +3,7 @@ import { screen, render } from "@testing-library/react";
 
 describe("SetGame", () => {
   test("it renders the Form component", () => {
+    render(<SetGame />);
     const { getByText } = render(<Form />);
     expect(getByText("Pick a category").toBeInTheDocument());
   });
