6d6005f792ca99737144246ee01c68995ffde132
Trina <92634994+trinayau@users.noreply.github.com>
2022-05-17 15:33:16 +0100

    fixed issue w lobby

--- frontend/src/pages/Lobby/index.js
+++ frontend/src/pages/Lobby/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { WaitingRoom } from "../../components";
 import "./style.css"
@@ -7,28 +6,6 @@ const Lobby = () => {
     <div className="lobby">
       <WaitingRoom />
     </div>
-=======
-import React, { useState, useEffect } from "react";
-import { WaitingRoom, Players } from "../../components";
-import { useSelector } from "react-redux";
-
-const Lobby = ({}) => {
-  const [players, setPlayers] = useState("");
-  const room = useSelector((state) => state.user.room);
-  const host = useSelector((state) => state.user.user.type);
-
-  return (
-    <>
-      <WaitingRoom host={host} />
-
-      <div id="players">
-        {players &&
-          players.map((player) => (
-            <Players key={players.indexOf(player)} player={player} />
-          ))}
-      </div>
-    </>
->>>>>>> 0ade8f4f18921d2cf505b5ab101306905263f2c2
   );
 };
 
