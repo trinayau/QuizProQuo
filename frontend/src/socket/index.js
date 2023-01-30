107395ae6ee4f5d84b3859a78784c6b57dd9755a
Trina <92634994+trinayau@users.noreply.github.com>
2023-01-30 13:16:02 +0000

    change socket heroku link to render

--- frontend/src/socket/index.js
+++ frontend/src/socket/index.js
@@ -1,6 +1,6 @@
 
 import io from "socket.io-client";
-const serverEndpoint = "https://quizpqsockets.herokuapp.com/";
+const serverEndpoint = "https://quizpq-sockets.onrender.com";
 const socket = io(serverEndpoint);
 
 export { socket };
